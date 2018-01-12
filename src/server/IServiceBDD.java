11d3b871fefb3b2e32c795bfb593e2efbe992b31
Matthieu31 <lenoirmatthieu@yahoo.fr>
2018-01-12 16:05:05 +0100

    Add files via upload
    Ajout des services liés à la table utilisateur_has_unread_ticket

--- src/server/IServiceBDD.java
+++ src/server/IServiceBDD.java
@@ -36,10 +36,10 @@ public interface IServiceBDD {
     public int nextIdMsg(int idFil,int idGrp);
     /**
      * Permet d'ajouter un fil de discussion dans la base de donnée
-     * @param idFil l'indentifiant unique de ce fil de discussion
+     * @param idFil l'indentifiant de ce fil de discussion
      * @param nom   le nom de ce fil de discussion
      * @param idGrp l'identifiant unique du groupe auquel appartient ce fil
-     * @param idMsg l'identifiant unique du premier message de ce fil
+     * @param idMsg l'identifiant du premier message de ce fil
      * @param contenuMsg le contenu du premier message de ce fil
      * @param date la date du premier message
      * @return un boléen indiquant la réussite de la création du fil
@@ -57,7 +57,7 @@ public interface IServiceBDD {
     public boolean ajouterMsg(int idMsg,String contenuMsg,int idFil,int idGrp,Date date);
     /**
     * Retourne la liste des messages d'un ticket présent dans la base de donnée
-    * @param idFil l'identifiant unique de ce fil
+    * @param idFil l'identifiant de ce fil
     * @param idGrp l'identifiant du groupe auquel appartient ce fil
     * @return la liste des messages de ce fil
     */
@@ -86,4 +86,26 @@ public interface IServiceBDD {
      * @return un booléen indiquant ou non la présence de l'utilisateur dans la base de donnée
      */
     public boolean utilisateurExiste(String idUti);
+    /**
+     * Permet d'associer un utilisateur à un message qu'il n'a pas encore lu dans la base de donnée
+     * @param idUti l'identifiant unique de l'utilisateur
+     * @param idFil l'identifiant du fil
+     * @param idGrp l'identifiant unique du groupe
+     * @return un booléen indiquant le succès ou non de l'ajout
+     */
+    public boolean ajouterTicketNonLu(String idUti,int idFil,int idGrp);
+    /**
+     * Permet de supprimer ticket non lu dans la base de donnée
+     * @param idUti l'identifiant unique de l'utilisateur
+     * @param idFil l'identifiant du fil
+     * @param idGrp l'identifiant unique du groupe
+     * @return un booléen indiquant le succès ou non de l'ajout
+     */
+    public boolean supprimerTicketNonLu(String idUti,int idFil,int idGrp);
+    /**
+     * Permet d'obtenir la liste des tickets qu'un utilisateur n'a pas encore lu
+     * @param idUti l'identifiant unique de l'utilisateur
+     * @return la liste des tickets non lu
+     */
+    public List <Ticket> listeTicketNonLu(String idUti);
 }
