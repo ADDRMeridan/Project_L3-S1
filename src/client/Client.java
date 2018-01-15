1fe5a7e1bdaf02ea0c063baf62759ccb92e406a9
ADDRMeridan <ADDRMeridan@users.noreply.github.com>
2018-01-15 05:05:08 +0100

    Add files via upload

---
+++ src/client/Client.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import network.ClientNet;
+import java.io.IOException;
+
+/**
+ *
+ * @author Mael "ADDRMeridan" Moulin
+ */
+public class Client {
+    
+    public static void main(String[] args) throws IOException {
+        
+        final ClientNet net = new ClientNet();
+        
+        try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(ClientUI.class.getName()).log(java.util.logging.Level.SEVERE, null,
+					ex);
+		}
+		// </editor-fold>
+
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+                            new ClientUI(net).setVisible(true);
+			}
+		});
+    }
+}
