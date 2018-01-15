c47915132687f76bb2e12c0481971d12d7bb5907
ADDRMeridan <ADDRMeridan@users.noreply.github.com>
2018-01-15 05:06:27 +0100

    Add files via upload

--- src/network/ObjectType.java
+++ src/network/ObjectType.java
@@ -3,5 +3,5 @@ package network;
 import java.io.Serializable;
 
 public enum ObjectType implements Serializable {
-	LOGIN, LOGIN_ANS, LOGOFF, NEW_TICKET, NEW_MESS, REFRESH_TREE, TREESET, OPEN_TICKET, LIST_MESS;
+	LOGIN, LOGIN_ANS, LOGOFF, NEW_TICKET, NEW_MESS, REFRESH_TREE, TREESET, OPEN_TICKET, LIST_MESS, ASK_GRP, LIST_GRP;
 }
