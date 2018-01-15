06bcfbd489742cda21ed151827819d325b48e1fe
Matthieu31 <lenoirmatthieu@yahoo.fr>
2018-01-15 18:27:16 +0100

    Add files via upload

--- src/server/GestionBDD.java
+++ src/server/GestionBDD.java
@@ -1,6 +1,6 @@
-package server;
+import package server;
 
-import java.sql.DriverManager;
+java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 
@@ -22,9 +22,9 @@ public class GestionBDD implements IGestionBDD {
 		} catch (ClassNotFoundException e) {
 			System.out.println(" Unable to load driver. ");
 		}
-                String url= "jdbc:mysql://localhost:8889/mydb";
-                String username="root";
-                String passd ="root";
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11215552";
+        String username = "sql11215552";
+        String passd = "HLaZ9P7drD";
 		try {
 			Connection conn = DriverManager.getConnection(url, username, passd);
 			Statement state = conn.createStatement();
@@ -53,9 +53,9 @@ public class GestionBDD implements IGestionBDD {
 		} catch (ClassNotFoundException e) {
 			System.out.println(" Unable to load driver. ");
 		}
-                String url= "jdbc:mysql://localhost:8889/mydb";
-                String username="root";
-                String passd ="root";
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11215552";
+        String username = "sql11215552";
+        String passd = "HLaZ9P7drD";
 		try {
 			Connection conn = DriverManager.getConnection(url, username, passd);
 			Statement state = conn.createStatement();
@@ -80,9 +80,9 @@ public class GestionBDD implements IGestionBDD {
 		} catch (ClassNotFoundException e) {
 			System.out.println(" Unable to load driver. ");
 		}
-                String url= "jdbc:mysql://localhost:8889/mydb";
-                String username="root";
-                String passd ="root";
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11215552";
+        String username = "sql11215552";
+        String passd = "HLaZ9P7drD";
 		try {
 			Connection conn = DriverManager.getConnection(url, username, passd);
 			Statement state = conn.createStatement();
@@ -133,9 +133,9 @@ public class GestionBDD implements IGestionBDD {
 		} catch (ClassNotFoundException e) {
 			System.out.println(" Unable to load driver. ");
 		}
-                String url= "jdbc:mysql://localhost:8889/mydb";
-                String username="root";
-                String passd ="root";
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11215552";
+        String username = "sql11215552";
+        String passd = "HLaZ9P7drD";
 		try {
 			Connection conn = DriverManager.getConnection(url, username, passd);
 			Statement state = conn.createStatement();
@@ -160,9 +160,9 @@ public class GestionBDD implements IGestionBDD {
 		} catch (ClassNotFoundException e) {
 			System.out.println(" Unable to load driver. ");
 		}
-                String url= "jdbc:mysql://localhost:8889/mydb";
-                String username="root";
-                String passd ="root";
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11215552";
+        String username = "sql11215552";
+        String passd = "HLaZ9P7drD";
 		try {
 			Connection conn = DriverManager.getConnection(url, username, passd);
 			Statement state = conn.createStatement();
@@ -193,9 +193,9 @@ public class GestionBDD implements IGestionBDD {
 		} catch (ClassNotFoundException e) {
 			System.out.println(" Unable to load driver. ");
 		}
-                String url= "jdbc:mysql://localhost:8889/mydb";
-                String username="root";
-                String passd ="root";
+        String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11215552";
+        String username = "sql11215552";
+        String passd = "HLaZ9P7drD";
 		try {
 			Connection conn = DriverManager.getConnection(url, username, passd);
 			Statement state = conn.createStatement();
